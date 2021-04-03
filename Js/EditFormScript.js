89c3686adcf9823d55a61ba883229509f62756de
Thái <68662500+Thai0108@users.noreply.github.com>
2021-04-03 23:04:35 +0700

    t

--- Js/EditFormScript.js
+++ Js/EditFormScript.js
@@ -373,7 +373,8 @@ sendForm.addEventListener('click', e => {
 getLink.addEventListener("click", e => {
     e.preventDefault();
     const el = document.createElement('textarea');
-    var url = window.location.href.replace("/html/EditForm.html", "/loadForm.html?fname=" + formId)
+    // var url = window.location.href.replace("/html/EditForm.html", "/loadForm.html?fname=" + formId)
+    var url = window.location.href.replace("EditForm.html", "/loadForm.html?fname=" + formId)
     el.value = url;
     document.body.appendChild(el);
     el.select();
